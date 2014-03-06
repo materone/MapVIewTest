7b9bdf5bba1c1f98ea232328e892b3479a1e3632
Materone <materone@gmail.com>
2014-03-07 01:10:14 +0800

    1
    1

--- MapVIewTest/RouteViewController.m
+++ MapVIewTest/RouteViewController.m
@@ -52,7 +52,7 @@
 
 - (IBAction)clear:(id)sender {
     [mapView removeOverlays:mapView.overlays];
-    [mapView removeAnnotations:annos];
+    [mapView removeAnnotations:mapView.annotations];
 }
 
 -(void)caculateRoute {
@@ -166,7 +166,7 @@
         MKPolylineView *lineview=[[MKPolylineView alloc] initWithOverlay:overlay] ;
         //路线颜色
         lineview.strokeColor=[UIColor colorWithRed:69.0f/255.0f green:212.0f/255.0f blue:255.0f/255.0f alpha:0.9];
-        lineview.lineWidth=2.0;
+        lineview.lineWidth=6.0;
         return lineview;
     }
     return nil;
