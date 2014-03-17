5d087eb2b8569a3b81d14635e70f661846fa9925
materone <materone@gmail.com>
2014-03-17 18:07:17 +0800

    a
    a

---
+++ MapVIewTest/BlockUIAlertView.h
@@ -0,0 +1,13 @@
+//
+//  BlockUIAlertView.h
+//  MapVIewTest
+//
+//  Created by tony on 14-3-17.
+//  Copyright (c) 2014年 tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BlockUIAlertView : UIAlertView
+
+@end
---
+++ MapVIewTest/BlockUIAlertView.m
@@ -0,0 +1,31 @@
+//
+//  BlockUIAlertView.m
+//  MapVIewTest
+//
+//  Created by tony on 14-3-17.
+//  Copyright (c) 2014年 tony. All rights reserved.
+//
+
+#import "BlockUIAlertView.h"
+
+@implementation BlockUIAlertView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
